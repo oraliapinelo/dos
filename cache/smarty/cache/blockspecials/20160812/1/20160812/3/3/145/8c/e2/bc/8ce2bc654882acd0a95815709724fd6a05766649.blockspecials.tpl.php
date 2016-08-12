05b37873436c7d6d97719b29abba8b91f5fc0296
alumno <alumno>
2016-08-12 03:19:03 -0500

    base de datos

---
+++ cache/smarty/cache/blockspecials/20160812/1/20160812/3/3/145/8c/e2/bc/8ce2bc654882acd0a95815709724fd6a05766649.blockspecials.tpl.php
@@ -0,0 +1,38 @@
+<?php /*%%SmartyHeaderCode:74257aad71cdb42e6-54634398%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '8ce2bc654882acd0a95815709724fd6a05766649' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\tienda\\themes\\theme593\\modules\\blockspecials\\blockspecials.tpl',
+      1 => 1470729851,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '74257aad71cdb42e6-54634398',
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_57ad7e27246605_18097010',
+  'has_nocache_code' => false,
+  'cache_lifetime' => 31536000,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_57ad7e27246605_18097010')) {function content_57ad7e27246605_18097010($_smarty_tpl) {?><!-- MODULE Block specials -->
+    <section id="specials" class="block products_block column_box">
+	<h4><span>Promociones especiales</span><span class="column_icon_toggle"></span></h4>
+	<div class="block_content toggle_content">
+		<ul>
+			<li class="shop_box  clearfix">
+				<a class="products_block_img" href="http://localhost/tienda/vestidos-verano/5-vestido-verano-estampado.html"><img src="http://localhost/tienda/12-small_default/vestido-verano-estampado.jpg" alt="" title="Vestido de verano estampado" /></a>
+				<div>
+				<h5><a class="product_link" href="http://localhost/tienda/vestidos-verano/5-vestido-verano-estampado.html" title="Vestido de verano estampado">Vestido de verano estampado</a></h5>
+				<p class="product_desc">Vestido largo estampado con tirantes...</p>                
+            	<span class="price">33,62 $</span>
+            																									<span class="reduction price">(-5%)</span>
+															
+				<span class="price-discount price">35,39 $</span>
+				</div>
+			</li>
+		</ul>
+		<a class="button_large" href="http://localhost/tienda/bajamos-precios" title="Todas los promociones especiales">Todas los promociones especiales</a>
+	</div>
+</section>
+<!-- /MODULE Block specials --><?php }} ?>
---
+++ cache/smarty/cache/blocktopmenu/3/3/145/category/14/e4/81/b5/e481b550c20751e6af6d2d7d3f8cd4083d1f56fc.blocktopmenu.tpl.php
@@ -0,0 +1,23 @@
+<?php /*%%SmartyHeaderCode:154557aad71b7eb673-85045524%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e481b550c20751e6af6d2d7d3f8cd4083d1f56fc' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\tienda\\themes\\theme593\\modules\\blocktopmenu\\blocktopmenu.tpl',
+      1 => 1470729851,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '154557aad71b7eb673-85045524',
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_57ad7e26f32f06_21957625',
+  'has_nocache_code' => false,
+  'cache_lifetime' => 31536000,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_57ad7e26f32f06_21957625')) {function content_57ad7e26f32f06_21957625($_smarty_tpl) {?><div id="menu-wrap">  
+	<ul id="menu-custom">
+			<li><a href="http://localhost/tienda/12-bolsas" title="Bolsas">Bolsas</a><ul><li class="sfHoverForce"><a href="http://localhost/tienda/14-de-mano" title="De mano">De mano</a></li></ul></li><li><a href="http://localhost/tienda/3-mujer" title="Mujer">Mujer</a><ul></ul></li>
+					</ul>
+	</div>
+<?php }} ?>
